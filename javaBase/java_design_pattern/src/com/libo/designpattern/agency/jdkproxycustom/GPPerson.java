60a365e4acdef7571799b54045f8d321b000669a
libo <790121301@qq.com>
2020-03-14 23:07:36 +0800

    删除多余的文件

--- javaBase/java_design_pattern/src/com/libo/designpattern/agency/jdkproxycustom/GPPerson.java
+++
@@ -1,9 +0,0 @@
-package com.libo.designpattern.agency.jdkproxycustom;
-
-public interface GPPerson {
-
-    void findLove();
-    void study();
-    String study1(String str);
-    //int study2(int i);
-}
